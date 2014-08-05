0212998de0cbaddbb2d6e00e301b40e303cc0838
Tibor Vass <teabee89@gmail.com>
2014-08-05 19:18:57 -0400

    complete rewrite
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- rsexpr.go
+++ rsexpr.go
@@ -1,17 +1,198 @@
-package rsexpr
+/*
+Package sexpr provides primitives to parse and format S-expressions with different notations.
+
+In addition to the LISP notation, there is also support for a new Tree notation
+that uses whitespace (tabs, spaces and newlines) to define structure.
+
+S-expressions are by recursive definition either an atom (usually a string), or a list of S-expressions.
+
+Example of an S-expression in LISP notation:
+	((a b (c (d e f) g)) (h i))
+
+Example of the same S-expression in Tree notation:
+	a
+		b
+		c
+			d e f
+			g
+	h i
+*/
+package sexpr
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"io"
+	"strings"
 )
 
-type Expr interface {
-	fmt.Stringer
-	SExpr() string
-	str(depth int) string
+// SExpr is a node in the S-expression.
+// If Children is nil, then it represents an atom, available in Value.
+// Otherwise, it represents a list of S-expressions.
+// If Value is nil, it is an empty node, which is diff
+type SExpr struct {
+	Value    *string
+	Children []*SExpr
 }
 
-const DEBUG = false
+// String returns an S-expression in the canonical Readable format.
+func (expr *SExpr) String() string {
+	//return expr.str(0)
+	return fmt.Sprintf("%p %s %#v", expr, expr.SExpr(), expr)
+	if expr.Children == nil {
+		if expr.Value != nil {
+			return *expr.Value
+		}
+		return ""
+	}
+
+	children := make([]string, len(expr.Children))
+	for i, child := range expr.Children {
+		children[i] = child.String()
+	}
+	return fmt.Sprintf("(%s)", strings.Join(children, " "))
+}
+
+func (expr *SExpr) str(depth int) string {
+	return ""
+	/*var b bytes.Buffer
+	t := make([]byte, depth)
+	d := 0
+	for ; d < depth; d++ {
+		t[d] = '\t'
+	}
+	b.Write(t)
+	if len(expr.Children) > 0 {
+		b.WriteString(expr.Children[0].str(d + 1))
+	}
+	sameLine := true
+	for i := 1; i < len(expr.Children); i++ {
+		if expr.Children[i].Children == nil {
+			sameLine = false
+			break
+		}
+	}
+	if sameLine {
+		for i := 1; i < len(expr.Children); i++ {
+			b.WriteByte(' ')
+			b.WriteString(expr.Children[i].str(d + 1))
+		}
+	} else {
+		for i := 1; i < len(expr.Children); i++ {
+			b.WriteByte('\n')
+			b.WriteString(expr.Children[i].str(d + 1))
+		}
+	}
+	return b.String()*/
+}
+
+// SExpr returns an S-expression with the parentheses notation.
+func (expr *SExpr) SExpr() string {
+	if expr.Children == nil {
+		if expr.Value != nil {
+			return *expr.Value
+		}
+		return ""
+	}
+
+	var b bytes.Buffer
+	b.WriteByte('(')
+	if len(expr.Children) > 0 {
+		b.WriteString(expr.Children[0].SExpr())
+	}
+	for i := 1; i < len(expr.Children); i++ {
+		b.WriteByte(' ')
+		b.WriteString(expr.Children[i].SExpr())
+	}
+	b.WriteByte(')')
+	return b.String()
+}
+
+func Parse(reader io.Reader) (root *SExpr, err error) {
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		inAtom := false
+		a := 0
+		for i, b := range data {
+			switch b {
+			case '(', ')':
+				if inAtom {
+					return i - 1, data[a:i], nil
+				}
+				return i, []byte{b}, nil
+			case ' ', '\t', '\n':
+				if inAtom {
+					return i - 1, data[a:i], nil
+				}
+				inAtom = false
+			default:
+				if !inAtom {
+					a = i
+				}
+				inAtom = true
+			}
+		}
+		// if we're at the end of the reader, return the one long atom
+		if atEOF {
+			return len(data), data[a:], nil
+		}
+		// ends while still scanning an atom (the atom might be longer)
+		if inAtom {
+			return 0, nil, nil
+		}
+		// no atoms nor parens
+		return len(data), []byte{}, nil
+	})
+	root = new(SExpr)
+	parseSExpr(scanner, root)
+	return root, nil
+}
+
+func parseSExpr(s *bufio.Scanner, expr *SExpr) {
+	for s.Scan() {
+		tok := s.Bytes()
+		if len(tok) == 0 {
+			break
+		}
+		if len(tok) == 1 {
+			switch tok[0] {
+			case '(':
+				if expr.Children == nil && expr.Value != nil {
+					expr.Children = append(expr.Children, &SExpr{Value: expr.Value})
+					expr.Value = nil
+				}
+			case ')':
+			}
+		}
+
+	}
+}
+
+// ParseTree parses an S-expression in the Tree notation from reader.
+func ParseTree(reader io.Reader) (root *SExpr, err error) {
+	defer func() {
+		if x := recover(); x != nil {
+			if e, ok := x.(error); ok {
+				err = e
+				return
+			}
+		}
+	}()
+	scanner := bufio.NewScanner(reader)
+	root = new(SExpr)
+	line := parseTree(scanner, nil, root, -1, false)
+	if root.Children == nil {
+		root = &SExpr{Children: []*SExpr{root}}
+	}
+	debug("--------")
+	if len(line) > 0 {
+		return root, fmt.Errorf("Recycled line: %q", line)
+	}
+	return root, nil
+}
+
+const DEBUG = true
 
 func debug(s string, args ...interface{}) {
 	if DEBUG {
@@ -19,156 +200,78 @@ func debug(s string, args ...interface{}) {
 	}
 }
 
-type atom string
-
-func (a atom) SExpr() string {
-	return string(a)
-}
-
-func (a atom) String() string {
-	return string(a)
-}
-
-func (a atom) str(depth int) string {
-	return string(a)
-}
-
-type list []Expr
-
-func (l list) SExpr() string {
-	var b bytes.Buffer
-	b.WriteByte('(')
-	if len(l) > 0 {
-		b.WriteString(l[0].SExpr())
-	}
-	for i := 1; i < len(l); i++ {
-		b.WriteByte(' ')
-		b.WriteString(l[i].SExpr())
-	}
-	b.WriteByte(')')
-	return b.String()
-}
-
-func (l list) String() string {
-	return l.str(0)
-}
-
-func (l list) str(depth int) string {
-	var b bytes.Buffer
-	t := make([]byte, depth)
-	d := 0
-	for ; d < depth; d++ {
-		t[d] = '\t'
-	}
-	b.Write(t)
-	if len(l) > 0 {
-		b.WriteString(l[0].str(d + 1))
-	}
-	sameLine := true
-	for i := 1; i < len(l); i++ {
-		if _, ok := l[i].(atom); !ok {
-			sameLine = false
-			break
-		}
-	}
-	if sameLine {
-		for i := 1; i < len(l); i++ {
-			b.WriteByte(' ')
-			b.WriteString(l[i].str(d + 1))
-		}
-	} else {
-		for i := 1; i < len(l); i++ {
-			b.WriteByte('\n')
-			b.WriteString(l[i].str(d + 1))
-		}
-	}
-	return b.String()
-}
-
-func Encode(data []byte) (expr Expr, err error) {
-	defer func() {
-		if x := recover(); x != nil {
-			var ok bool
-			if err, ok = x.(error); ok {
-				expr = nil
-				return
-			}
-			panic(x)
-		}
-	}()
-	e, _ := parseColumn(data, list{}, 0, 0)
-	return e, nil
-}
-
-func parseColumn(data []byte, l list, depth, i int) (Expr, int) {
-	debug("parseColumn: i=%d depth=%d l=%v", i, depth, l)
-	var line Expr
-	d := depth
-	for i < len(data) {
-		line, i = parseLine(data, list{}, depth, i)
-		debug("line=%v", line)
-		d = 0
-		for i < len(data) && data[i] == '\t' {
-			debug("i=%d, c=%q", i, data[i])
-			i++
-			d++
-		}
-		debug("%d > %d", d, depth)
-		if d > depth {
-			if d == depth+1 {
-				line, i = parseColumn(data, line.(list), d, i)
-			} else {
-				var e Expr
-				e, i = parseColumn(data, list{}, d, i)
-				for n := depth + 2; n < d; n++ {
-					e = list{e}
-				}
-				line = append(line.(list), e)
-			}
-		}
-		if L, ok := line.(list); ok {
-			if len(L) == 1 {
-				line = L[0]
-			}
-		}
-		l = append(l, line)
-		debug("update l = %v", l)
-		if d < depth {
-			debug("end of parseColumn: i=%d l=%v", i, l)
-			return l, i
-		}
-	}
-
-	debug("End of parseColumn: i=%d l=%v", i, l)
-	return l, i
-}
-
-func parseLine(data []byte, l list, depth, i int) (Expr, int) {
-	debug("parseLine: i=%d depth=%d l=%v", i, depth, l)
-	a := i
-	word := false
-	for i < len(data) {
-		debug("i=%d, c=%q", i, data[i])
-		switch data[i] {
-		case ' ', '\t':
-			if word {
-				l = append(l, atom(data[a:i]))
-				word = false
-			}
-		case '\n':
-			debug("l=%v", l)
-			l = append(l, atom(data[a:i]))
-			debug("\\n end of parseLine: i=%d l=%v", i+1, l)
-			return l, i + 1
-		default:
-			if !word {
-				a = i
-				word = true
-			}
-		}
+func parseLine(line []byte) (expr *SExpr, depth int) {
+	i := 0
+	for i < len(line) && line[i] == '\t' {
 		i++
 	}
-	l = append(l, atom(data[a:i]))
-	debug("end of parseLine: i=%d l=%v", i, l)
-	return l, i
+	depth = i
+	if i == len(line) {
+		return
+	}
+	atoms := bytes.Fields(line[i:])
+	if len(atoms) == 1 {
+		s := string(atoms[0])
+		expr = &SExpr{Value: &s}
+		return
+	}
+	expr = &SExpr{Children: make([]*SExpr, len(atoms))}
+	for n, atom := range atoms {
+		s := string(atom)
+		expr.Children[n] = &SExpr{Value: &s}
+	}
+	return
+}
+
+func parseTree(s *bufio.Scanner, recycled []byte, expr *SExpr, depth int, wasOneLine bool) []byte {
+	debug("expr=%s", expr)
+	line := recycled
+	for len(line) > 0 || s.Scan() {
+		if len(line) == 0 {
+			// if no recycled lines, read new lines
+			line = s.Bytes()
+		}
+		if len(line) == 0 {
+			// skip empty lines
+			continue
+		}
+
+		node, lineDepth := parseLine(line)
+		debug("lineDepth=%d n=%s", lineDepth, node)
+		if lineDepth <= depth {
+			// TODO: reuse node instead of line
+			// end of tree
+			// recycle line
+			return line
+		}
+
+		if lineDepth > depth+1 {
+			// add current node to an empty node that will be added to expr
+			// recycle line
+			node = new(SExpr)
+		} else {
+			// add current node to expr
+			// no need to recycle anything
+			line = nil
+		}
+
+		// recursion
+		line = parseTree(s, line, node, depth+1, node.Children != nil)
+
+		// atom -> (atom) except if empty node
+		if expr.Children == nil && expr.Value != nil {
+			expr.Children = append(expr.Children, &SExpr{Value: expr.Value})
+			expr.Value = nil
+		}
+
+		// append node to (atom): (atom NODE)
+		expr.Children = append(expr.Children, node)
+		debug("new expr=%s", expr)
+	}
+	// if one-atom list, then unwrap:
+	// (atom) -> atom
+	if len(expr.Children) == 1 {
+		*expr = *expr.Children[0]
+	}
+	return nil
 }
--- rsexpr_test.go
+++ rsexpr_test.go
@@ -1,11 +1,37 @@
-package rsexpr
+package sexpr
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
-func Test1(t *testing.T) {
+func TestParseReadable(t *testing.T) {
 	tests := [][2]string{
-		{
-			`a
+		{`a`, `(a)`},
+		{`a b`, `(a b)`},
+		{`
+a
+b`,
+			`(a b)`},
+		{`
+a b
+c`,
+			`((a b) c)`},
+		{`
+a
+		b
+c
+			d
+				e
+			f
+	g`,
+			`((a (b)) (c (((d e) f)) g))`},
+		{`
+a
+	b c`,
+			`(a (b c))`},
+		{`
+a
 	b
 		c d e
 		f
@@ -14,7 +40,8 @@ h`,
 			`((a (b (c d e) f) g) h)`,
 		},
 		{
-			`a
+			`
+a
 		b c d
 		e
 				f
@@ -24,7 +51,8 @@ h`,
 			`(a ((b c d) (e (f g) h)) i)`,
 		},
 		{
-			`a
+			`
+a
 	b
 		c d e
 		f
@@ -32,29 +60,37 @@ g`,
 			`((a (b (c d e) f)) g)`,
 		},
 		{
-			`a
+			`
+a
 	b c d
 		e
 	f
 g`,
-			`invalid`,
+			`((a ((b c d) (e) f)) g)`,
 		},
 	}
 
-	for _, test := range tests {
-		expr, err := Encode([]byte(test[0]))
+	for _, test := range tests[len(tests)-1:] {
+		expr, err := ParseReadable(strings.NewReader(test[0]))
 		t.Logf("Testing: %s", test[1])
 		if (err == nil) == (test[1] == "invalid") {
 			t.Error(err)
 		}
 		if expr.SExpr() != test[1] {
 			t.Log("FAILED")
-			t.Log(expr)
+			t.Logf("%s", expr)
+			t.Logf("%q", expr.SExpr())
 			t.Log("got:     ", expr.SExpr())
 			t.Errorf("expected: %s", test[1])
 		} else {
 			t.Log("PASSED")
 		}
+		/*if expr.String() != test[0] {
+			t.Log("FAILED")
+			t.Logf("%p %#v", expr, expr)
+			t.Logf("got:     %q", expr.String())
+			t.Errorf("expected: %q", test[0])
+		}*/
 		t.Log("-------------")
 	}
 }
